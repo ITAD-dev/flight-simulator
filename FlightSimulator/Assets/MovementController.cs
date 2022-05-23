abddb499005661b3e5a6d75df15be20e16fd67fe
Dante Falzone <dantefalzone0@protonmail.com>
2022-05-22 23:03:59 -0500

    Started writing MovementController C# class

---
+++ FlightSimulator/Assets/MovementController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MovementControl: MonoBehaviour {
+  // Start is called before the first frame update
+  void Start() {
+    // TODO
+  }
+
+  // Update is called once per frame
+  void Update() {
+    
+  }
+}
