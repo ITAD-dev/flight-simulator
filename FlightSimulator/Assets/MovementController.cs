eeda95176ef73727db0cd1db95b40addf6098182
Dante Falzone <dantefalzone0@protonmail.com>
2022-05-23 00:49:58 -0500

    implemented gravity and vertical acceleration

--- FlightSimulator/Assets/MovementController.cs
+++ FlightSimulator/Assets/MovementController.cs
@@ -1,13 +1,41 @@
 using UnityEngine;
 
-public class MovementControl: MonoBehaviour {
+public class MovementController: MonoBehaviour {
+  public float gravitationalConstant;
+  public float autonomousVerticalAcceleration; // rate at which the vehicle can propel itself up
+  public float maximumUpwardVelocity;
+  private float currentUpwardVelocity;
+
   // Start is called before the first frame update
   void Start() {
-    // TODO
+    this.currentUpwardVelocity = 0;
   }
 
   // Update is called once per frame
   void Update() {
-    
+    // Pull the vehicle down if it is above the ground
+    if (this.transform.position.y >= 0) {
+      this.transform.position += new Vector3(
+        0, // x axis
+        this.currentUpwardVelocity * Time.deltaTime, // y axis
+        0 // z axis
+      );
+    } else {
+      this.transform.position = new Vector3(
+        this.transform.position.x,
+        0,
+        this.transform.position.z
+      );
+    }
+
+    if (Input.GetKey(KeyCode.Space)
+        && this.currentUpwardVelocity < this.maximumUpwardVelocity) {
+      this.currentUpwardVelocity += this.autonomousVerticalAcceleration;
+    } else if (this.transform.position.y > 0) {
+      this.currentUpwardVelocity -= this.gravitationalConstant;
+    } else {
+      this.currentUpwardVelocity = 0;
+    }
+
   }
 }
